0fa319ee76fc2db2638bdfe868908ba0e806f248
Jake Fennick <jake.fennick@axleinfo.com>
2022-04-18 11:36:42 -1000

    fixed realtime plots window foreground/background issue

--- WatchdogPlots.py
+++ WatchdogPlots.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import sys
 import time
-from typing import Any
+from typing import List, Tuple
+import matplotlib
 
 import numpy as np
+from matplotlib import _pylab_helpers # type: ignore
 import matplotlib.pyplot as plt
 
 from watchdog.observers import Observer
@@ -20,34 +22,47 @@ def read_tabular_data(filename: str) -> np.ndarray:
     return arr
 
 
-# Create blank placeholder subplots
-nrows: int = 2
-ncols: int = 3
-plt.style.use('dark_background')
-fig, axes2d = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 9))
-fig.suptitle('')
-fig.tight_layout()
-plt.subplots_adjust(left=0.08, wspace=0.24, bottom=0.08, hspace=0.24)
-# NOTE: "The pause is needed because the GUI events happen while the main code is sleeping, including drawing."
-# See https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
-plt.pause(0.1)
-plt.show(block=False) # type: ignore
-
-
 # Declare global variables to pass data between the main thread and the observer thread. (Not ideal)
 idx_glob = -1
-data_glob = None
-path_glob = None
+data_glob: np.ndarray = np.array([])
+path_glob: Path
 
 
-def plot_new_data():
+def initialize_plots(nrows: int, ncols: int) -> Tuple[matplotlib.pyplot.Figure, List[List[matplotlib.pyplot.Axes]]]:
+    """Initialize a grid of blank subplots.
+
+    Args:
+        nrows (int): the number of rows
+        ncols (int): the number of columns
+
+    Returns:
+        Tuple[matplotlib.pyplot.Figure, List[List[matplotlib.pyplot.Axes]]]: The main figure and subplots axes
+    """
+    plt.style.use('dark_background') # type: ignore
+    fig, axes2d = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 9))
+    fig.suptitle('')
+    fig.tight_layout()
+    plt.subplots_adjust(left=0.08, wspace=0.24, bottom=0.08, hspace=0.24)
+    plt.show(block=False) # type: ignore
+    return (fig, axes2d)
+
+
+def update_plots(fig: matplotlib.pyplot.Figure, axes2d: List[List[matplotlib.pyplot.Axes]]) -> None:
+    """Update the previously initialized plots (if there is any new data).
+
+    Args:
+        fig (matplotlib.pyplot.Figure): The main figure
+        axes2d (List[List[matplotlib.pyplot.Axes]]): The subplots axes
+    """
+    nrows = len(axes2d)
+    ncols = len(axes2d[0])
     # Declare global variables locally
     global idx_glob
     global data_glob
     global path_glob
-    if isinstance(data_glob, np.ndarray):
+    if data_glob.size > 0:
         data = data_glob
-        data_glob = None
+        data_glob = np.array([])
         idx = 0
         for axes1d in axes2d:
             for ax_ in axes1d:
@@ -62,24 +77,22 @@ def plot_new_data():
                     #print(ys)
                     ax.set_xlim(min(xs), max(xs))
                     ax.set_ylim(min(ys), max(ys))
-                    ax.ticklabel_format(style='sci', scilimits=(-2,3), axis='both')
+                    ax.ticklabel_format(style='sci', scilimits=(-2,3), axis='both') # type: ignore
                     # See https://stackoverflow.com/questions/39753282/scatter-plot-with-single-pixel-marker-in-matplotlib
                     ax.scatter(xs, ys, marker='o', s=(72./fig.dpi)**2)  # type: ignore
-                    ax.set_title(Path(path_glob).stem + '.xvg')
+                    ax.set_title(path_glob.stem + '.xvg')
                     ax.set_xlabel('x-axis')
                     ax.set_ylabel('y-axis')
-                    plt.draw()
-                    plt.pause(0.1)
                 idx += 1
 
 
 class XvgHandler(PatternMatchingEventHandler):
 
-    def __init__(self):
+    def __init__(self) -> None:
         patterns = ['*.xvg']
         super().__init__(patterns)
 
-    def on_any_event(self, event: FileSystemEvent) -> Any:
+    def on_any_event(self, event: FileSystemEvent) -> None:
         # Declare global variables locally
         global idx_glob
         global data_glob
@@ -88,19 +101,43 @@ class XvgHandler(PatternMatchingEventHandler):
             #print(event)
             idx_glob += 1
             data_glob = read_tabular_data(event.src_path)
-            path_glob = event.src_path
+            path_glob = Path(event.src_path)
 
 
-def main():
+def pause_no_show(interval: float) -> None:
+    """This is verbatim copy and pasted from matplotlib.pyplot.pause(), with show() commented out.
+    show() rudely brings the window to the foreground, which interrupts the user.
+
+    Args:
+        interval (float): 
+    """
+    manager = _pylab_helpers.Gcf.get_active()
+    if manager is not None:
+        canvas = manager.canvas
+        if canvas.figure.stale:
+            canvas.draw_idle()
+        #show(block=False)
+        canvas.start_event_loop(interval)
+    else:
+        time.sleep(interval)
+
+
+def main() -> None:
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = XvgHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
+
+    nrows: int = 2
+    ncols: int = 3
+    (fig, axes2d) = initialize_plots(nrows, ncols)
     try:
         while True:
-            time.sleep(1)
-            plot_new_data()
+            # NOTE: Do NOT use time.sleep(0.1) here!
+            # It does NOT restart the GUI event loop!
+            pause_no_show(0.1)
+            update_plots(fig, axes2d)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
