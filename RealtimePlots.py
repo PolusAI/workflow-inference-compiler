60ac9b4fa0936038f602bac0751d71070803c3c1
Jake Fennick <jake.fennick@axleinfo.com>
2022-04-29 05:35:37 -1000

    added spline smoothing and Change Point Detection to realtime plots

--- RealtimePlots.py
+++ RealtimePlots.py
@@ -9,6 +9,9 @@ import matplotlib
 import numpy as np
 from matplotlib import _pylab_helpers # type: ignore
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+
+import ruptures as rpt
 
 """from watchdog.observers import Observer
 from watchdog.observers.polling import PollingObserver
@@ -73,10 +76,10 @@ def initialize_plots(nrows: int, ncols: int) -> Tuple[matplotlib.pyplot.Figure, 
         Tuple[matplotlib.pyplot.Figure, List[List[matplotlib.pyplot.Axes]]]: The main figure and subplots axes
     """
     plt.style.use('dark_background') # type: ignore
-    fig, axes2d = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 9))
+    fig, axes2d = plt.subplots(nrows=nrows, ncols=ncols, figsize=(18, 9.5))
     fig.suptitle('')
     fig.tight_layout()
-    plt.subplots_adjust(left=0.08, wspace=0.24, bottom=0.08, hspace=0.24)
+    plt.subplots_adjust(left=0.05, wspace=0.24, bottom=0.05, hspace=0.24)
     plt.show(block=False) # type: ignore
     return (fig, axes2d)
 
@@ -87,9 +90,9 @@ labels = {
     'energy_min_cg.xvg': ('Conjugate Gradient Minimization', 'timesteps', 'energy (kJ / mol)'),
     'temperature.xvg': ('Temperature', 'time (ps)', 'temperature (K)'),
     'density.xvg': ('Density', 'time (ps)', 'density (g / cm^3)'),
-    'rmsd_xray.xvg': ('Root Mean Square w.r.t. Xray', 'time (ps)', 'rmsd (A)'),
-    'rmsd_equil.xvg': ('Root Mean Square w.r.t. Equil', 'time (ps)', 'rmsd (A)'),
-    'radius_gyration.xvg': ('Radius of Gyration', 'time (ps)', 'radius (A)'),
+    'rmsd_xray.xvg': ('Root Mean Square w.r.t. Xray', 'time (ps)', 'rmsd (nm)'),
+    'rmsd_equil.xvg': ('Root Mean Square w.r.t. Equil', 'time (ps)', 'rmsd (nm)'),
+    'radius_gyration.xvg': ('Radius of Gyration', 'time (ps)', 'radius (nm)'),
 }
 
 
@@ -107,48 +110,87 @@ def update_plots(fig: matplotlib.pyplot.Figure, axes2d: List[List[matplotlib.pyp
     global data_glob_changed
     if data_glob_changed:
         #print('updating plots')
-        idx = 0
-        for axes1d in axes2d:
-            for ax_ in axes1d:
-                if idx < len(data_glob):
-                    (path, data) = data_glob[idx]
-                    idx_col = int(idx / ncols)
-                    idx_row = idx - (ncols * idx_col)
-                    ax = axes2d[idx_col][idx_row]
-                    if len(data.shape) == 2:
-                        xs = data[:,0]
-                        ys = data[:,1]
-                        # TODO: Decide how to plot multiple y values.
-                    elif len(data.shape) == 1:
-                        #print('path', path)
-                        #print('data', data)
-                        ys = data[:]
-                        xs = np.array([x for x in range(len(ys))])
-                    else:
-                        print('Error! data.shape is not of length 1 or 2:', data.shape)
-                        print('path', path)
-                        idx += 1
-                        continue
-                        
-                    #print(path)
-                    #print(xs)
-                    #print(ys)
-                    ax.set_xlim(min(xs), max(xs))
-                    ax.set_ylim(min(ys), max(ys))
+        idx_ax = 0
+        for path, data in data_glob:
+            #print(num y cols, len(data[0]))
+            # Plot multiple y values individually
+            for idx_y_col in range(1, len(data[0])):
+                if len(data.shape) == 2:
+                    xs = data[:,0]
+                    ys = data[:,idx_y_col] 
+                elif len(data.shape) == 1:
+                    #print('path', path)
+                    #print('data', data)
+                    ys = data[:]
+                    xs = np.array([x for x in range(len(ys))])
+                else:
+                    print('Error! data.shape is not of length 1 or 2:', data.shape)
+                    print('path', path)
+                    continue
+                #print(path)
+                #print(xs)
+                #print(ys)
+
+                plot_histograms_bool = 'rmsd' in Path(path).stem
+                plot_histograms_list = [False, True] if plot_histograms_bool else [False]
+
+                for plot_histogram in plot_histograms_list:
+                    # Figure out the axes
+                    idx_ax_col = int(idx_ax / ncols)
+                    idx_ax_row = idx_ax - (ncols * idx_ax_col)
+                    ax = axes2d[idx_ax_col][idx_ax_row]
                     ax.ticklabel_format(style='sci', scilimits=(-2,3), axis='both') # type: ignore
-                    # See https://stackoverflow.com/questions/39753282/scatter-plot-with-single-pixel-marker-in-matplotlib
-                    ax.scatter(xs, ys, marker='o', s=(72./fig.dpi)**2, c='blue')  # type: ignore
-                    filename = Path(path).stem + '.xvg'
-                    if filename in labels:
-                        (title, xlabel, ylabel) = labels[filename]
-                        ax.set_title(title)
-                        ax.set_xlabel(xlabel)
-                        ax.set_ylabel(ylabel)
+                    if plot_histogram:
+                        ax.hist(ys, density=True, histtype='step', bins='sqrt', color='blue') # type: ignore
+                        ax.set_xlim(xmin=0)
+
+                        filename = Path(path).stem + '.xvg'
+                        if filename in labels:
+                            (title, xlabel, ylabel) = labels[filename]
+                            ax.set_title(title)
+                            ax.set_xlabel(ylabel)
+                            ax.set_ylabel('frequency')
+                        else:
+                            ax.set_title(filename)
+                            ax.set_xlabel('y-axis')
+                            ax.set_ylabel('frequency')
                     else:
-                        ax.set_title(filename)
-                        ax.set_xlabel('x-axis')
-                        ax.set_ylabel('y-axis')
-                idx += 1
+                        ymin = min(ys)
+                        ymax = max(ys)
+                        ax.set_xlim(min(xs), max(xs))
+                        ax.set_ylim(ymin, ymax)
+
+                        # See https://stackoverflow.com/questions/39753282/scatter-plot-with-single-pixel-marker-in-matplotlib
+                        ax.scatter(xs, ys, marker='o', s=(72./fig.dpi)**2, c='blue')  # type: ignore
+
+                        # Smooth the timeseries using spline interpolaton
+                        spline = UnivariateSpline(xs, ys)
+                        spline.set_smoothing_factor(0.15)
+                        ys_spline = spline(xs)
+                        ax.plot(xs, ys_spline, color='g')
+
+                        # Use Change Point Detection to partition the timeseries
+                        # into piecewise 'constant' segments.
+                        # Pure python implementation is very slow!
+                        # algo = rpt.Pelt(model='rbf', min_size=10).fit(ys)
+                        # C implementation is much faster
+                        algo = rpt.KernelCPD(kernel='rbf', min_size=10).fit(ys)
+                        indices = algo.predict(pen=100) # Large penalty = less segments
+                        changepoints = [xs[i-1] for i in indices]
+                        for cp_xval in changepoints:
+                            ax.vlines(cp_xval, ymin, ymax, color='gray') # type: ignore
+
+                        filename = Path(path).stem + '.xvg'
+                        if filename in labels:
+                            (title, xlabel, ylabel) = labels[filename]
+                            ax.set_title(title)
+                            ax.set_xlabel(xlabel)
+                            ax.set_ylabel(ylabel)
+                        else:
+                            ax.set_title(filename)
+                            ax.set_xlabel('x-axis')
+                            ax.set_ylabel('y-axis')
+                    idx_ax += 1
         data_glob_changed = False
 
 
@@ -204,8 +246,15 @@ def file_watcher_glob(dir: Path, pattern: str, prev_files: Dict[str, float]) -> 
             changed_files[file] = time
     return changed_files
 
+figure_closed = False
+
+def on_close(event) -> None: # type: ignore
+    global figure_closed
+    figure_closed = True
+
 
 def main() -> None:
+    global figure_closed
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
 
     #event_handler = TabularDataHandler(patterns=['*.xvg'])
@@ -213,15 +262,21 @@ def main() -> None:
     #observer.schedule(event_handler, path, recursive=True)
     #observer.start()
 
-    nrows: int = 2
-    ncols: int = 3
+    nrows: int = 3
+    ncols: int = 4
     (fig, axes2d) = initialize_plots(nrows, ncols)
+    fig.canvas.mpl_connect('close_event', on_close) # type: ignore
     prev_files: Dict[str, float] = {}
     try:
-        while True:
+        while not figure_closed:
             # Use our own polling file watcher, see above.
             changed_files = file_watcher_glob(Path(path), '*.xvg', prev_files)
-            for file in changed_files:
+            changed_files_list = list(changed_files.items())
+            # Sort by modified time so that if we start RealtimePlots in the
+            # middle of the calculation, the plots will still be drawn in the
+            # same order.
+            changed_files_list.sort(key=lambda x: x[1])
+            for file, time_ in changed_files_list:
                 print(file)
                 store_tabular_data(Path(file))
             prev_files = {**prev_files, **changed_files}
