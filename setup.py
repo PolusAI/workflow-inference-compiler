d4cbb17bf8cba42cb6414627a650f776da35aeea
Brandon Walker <43654521+misterbrandonwalker@users.noreply.github.com>
2024-01-12 14:26:11 -0600

    Combine configuration files into pyproject.toml (#145)
    * Add pyproject.toml for wic
    
    * upgrade versioneer
    
    ---------
    
    Co-authored-by: Brandon Duane Walker <walkerbd@UPDATEME.HOSTNAME.COM>

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import setuptools
-import versioneer
-
-if __name__ == "__main__":
-    setuptools.setup(version=versioneer.get_version(),
-                     cmdclass=versioneer.get_cmdclass())
