0c318f8143d5c81bf1eb86bf1b1549f491383775
Camilo Velez <camilov61@gmail.com>
2024-02-27 19:24:39 -0500

    feat: add compile_step/workflow

--- src/wic/api/__init__.py
+++ src/wic/api/__init__.py
@@ -1,3 +1,3 @@
-from .pythonapi import Step, Workflow
+from .pythonapi import Step, Workflow, configure_step, configure_workflow
 
-__all__ = ["Step", "Workflow"]
+__all__ = ["Step", "Workflow", "configure_step", "configure_workflow"]
