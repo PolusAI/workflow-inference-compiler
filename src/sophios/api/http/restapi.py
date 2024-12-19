8114a4dc9d75a3d8f41e673efdcae94501eb6b27
Sameeul Samee <sameeul@gmail.com>
2024-12-19 02:14:51 -0500

    drop uuid from workflow name

--- src/sophios/api/http/restapi.py
+++ src/sophios/api/http/restapi.py
@@ -102,7 +102,7 @@ async def compile_wf(request: Request) -> Json:
     wfb_payload = converter.raw_wfb_to_lean_wfb(req)
     # schema non-preserving
     workflow_temp = converter.wfb_to_wic(wfb_payload, req["plugins"])
-    wkflw_name = "generic_workflow_" + str(uuid.uuid4())
+    wkflw_name = "workflow_"
     args = get_args(wkflw_name, suppliedargs)
 
     # Build canonical workflow object
